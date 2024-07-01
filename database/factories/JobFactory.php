389b0a10984bf8f93b8c64040074039cf099237e
TalalBnoob <79761207+TalalBnoob@users.noreply.github.com>
2024-07-01 11:08:26 +0300

    Cooooooding

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory {
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition(): array {
+		return [
+			"employer_id" => Employer::factory(),
+			'title' => fake()->jobTitle,
+			'salary' => "$" . fake()->randomElement(["50,000", "60,000", "70,000", "80,000", "95,000", "100,000", "120,000"]) . " USD",
+			'location' => "Remote",
+			'Schedule' => fake()->randomElement(["Full Time", "Part Time"]),
+			'url' => fake()->url,
+			'featured' => fake()->boolean
+		];
+	}
+}
